d6a7db75e117db78f1cc2f270a5ba465ba669f88
Nagyvee <nigelmafuta1@gmail.com>
2024-07-20 23:15:58 +0200

    save

---
+++ runOpt.js
@@ -0,0 +1,25 @@
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+
+const inputDir = './before';
+const outputDir = './';
+
+if (!fs.existsSync(outputDir)){
+  fs.mkdirSync(outputDir);
+}
+
+fs.readdirSync(inputDir).forEach(file => {
+  const inputFile = path.join(inputDir, file);
+  const outputFile = path.join(outputDir, file.replace(/\.[^/.]+$/, ".webp"));
+
+  sharp(inputFile)
+    .toFormat('webp')
+    .toFile(outputFile, (err, info) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`Converted: ${file}`);
+      }
+    });
+});
